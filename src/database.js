d7586c22dd4f30794f00bac57eb737c499288980
anbreaker <antunez19@gmail.com>
2020-05-24 21:14:15 +0200

    probando heroku

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+// const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+const MONGODB_URI = `mongodb+srv://anbreaker:anbreaker_mongo@cluster0-l42v7.mongodb.net/test?retryWrites=true&w=majority`;
 
 // Configurate tool mongoose to conet mongodb
 mongoose

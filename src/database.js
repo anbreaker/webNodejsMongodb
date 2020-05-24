6b145dd5b5e0bad4bdf085ca2a67144cadbd92f9
anbreaker <antunez19@gmail.com>
2020-05-24 21:36:32 +0200

    probando modulo para netlify.

--- src/database.js
+++ src/database.js
@@ -4,7 +4,6 @@ const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
 const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
 // Configurate tool mongoose to conet mongodb
-mongoose.connect(process.env.MONGOLAB_URI);
 mongoose
   .connect(MONGODB_URI, {
     useUnifiedTopology: true,
